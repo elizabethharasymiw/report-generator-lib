c7965dc79dc0415b25ab485e834fdb081ba291a2
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-08-12 11:39:48 -0500

    Add function to create a hardcoded pdf

--- report_generator_lib/__init__.py
+++ report_generator_lib/__init__.py
@@ -1,1 +1,2 @@
 from .pdf_report import add
+from .pdf_report import freecad_assistant_pdf_report
--- report_generator_lib/pdf_report.py
+++ report_generator_lib/pdf_report.py
@@ -1,3 +1,21 @@
+from fpdf import FPDF
+
 def add(a, b):
     """Return the sum of two numbers."""
     return a + b
+
+def freecad_assistant_pdf_report():
+    # Create instance of FPDF class
+    pdf = FPDF()
+
+    # Add a page
+    pdf.add_page()
+
+    # Set Title font
+    pdf.set_font("Arial", style='B', size=32)
+
+    # Add a cell
+    pdf.cell(200, 10, txt="Welcome to FPDF!", ln=True, align='C')
+
+    # Save the PDF with name .pdf
+    pdf.output("example.pdf")
