36e863ee16594f30f94f4267e2597ca065257bbb
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-08-12 10:46:42 -0500

    Create simple example package of an add function

---
+++ report_generator_lib/__init__.py
@@ -0,0 +1,1 @@
+from .pdf_report import add
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='report-generator-lib',
+    version='0.1',
+    description='Collection of functions to generate reports',
+    author='Elizabeth Harasymiw',
+    packages=find_packages(),
+    install_requires=[],
+)
