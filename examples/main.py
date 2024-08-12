e2057550934dbece436fb361cead72cfa905daf1
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-08-12 11:40:30 -0500

    Add call to pdf generation function

--- examples/main.py
+++ examples/main.py
@@ -1,10 +1,14 @@
 from report_generator_lib import add
+from report_generator_lib import freecad_assistant_pdf_report
 
 def main():
     num1 = 5
     num2 = 7
     result = add(num1, num2)
     print(f"The sum of {num1} and {num2} is {result}")
+
+    # Create FreeCAD assistant PDF report
+    freecad_assistant_pdf_report()
 
 if __name__ == "__main__":
     main()
