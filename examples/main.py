00de1271ee2350ceed7f813bf6f28ae111a45c38
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-08-12 10:47:47 -0500

    Add example of using add function from example package

---
+++ examples/main.py
@@ -0,0 +1,10 @@
+from report_generator_lib import add
+
+def main():
+    num1 = 5
+    num2 = 7
+    result = add(num1, num2)
+    print(f"The sum of {num1} and {num2} is {result}")
+
+if __name__ == "__main__":
+    main()
